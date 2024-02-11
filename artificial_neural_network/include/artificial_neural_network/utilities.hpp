b081dbf7e486c982c8e2ad000b96cc97102ed68f
Oleksandr Nemchenko <avn@nemchenko.net>
2024-02-11 17:28:24 +0200

    Add the ability to configure all data types

--- artificial_neural_network/include/artificial_neural_network/utilities.hpp
+++ artificial_neural_network/include/artificial_neural_network/utilities.hpp
@@ -7,24 +7,79 @@
 #include <vector>
 
 #include <halfFloat.hpp>
+#include <annDataTypes.hpp>
 
 namespace artificial_neural_network
 {
 
-using data_type = float16;
-using offset_type = uint32_t;
+#if ANN_INPUT_TYPE_BYTES == 1
+    using input_type = char;
+#elif ANN_INPUT_TYPE_BYTES == 2
+    using input_type = float16;
+#elif ANN_INPUT_TYPE_BYTES == 4
+    using input_type = float;
+#elif ANN_INPUT_TYPE_BYTES == 8
+    using input_type = double;
+#endif // ANN_INPUT_TYPE_BYTES
 
-using datas_type = std::vector<data_type>;
-using offsets_type = std::vector<offset_type>;
+#if ANN_OUTPUT_TYPE_BYTES == 1
+    using output_type = char;
+#elif ANN_OUTPUT_TYPE_BYTES == 2
+    using output_type = float16;
+#elif ANN_OUTPUT_TYPE_BYTES == 4
+    using output_type = float;
+#elif ANN_OUTPUT_TYPE_BYTES == 8
+    using output_type = double;
+#endif // ANN_OUTPUT_TYPE_BYTES
 
-inline offset_type ConvertToOffsetType(const size_t offset)
+#if ANN_NEURON_STATE_TYPE_BYTES == 1
+    using neuron_state_type = char;
+#elif ANN_NEURON_STATE_TYPE_BYTES == 2
+    using neuron_state_type = float16;
+#elif ANN_NEURON_STATE_TYPE_BYTES == 4
+    using neuron_state_type = float;
+#elif ANN_NEURON_STATE_TYPE_BYTES == 8
+    using neuron_state_type = double;
+#endif // ANN_NEURON_STATE_TYPE_BYTES
+
+#if ANN_NEURON_CONFIG_TYPE_BYTES == 1
+    using neuron_config_type = char;
+#elif ANN_NEURON_CONFIG_TYPE_BYTES == 2
+    using neuron_config_type = float16;
+#elif ANN_NEURON_CONFIG_TYPE_BYTES == 4
+    using neuron_config_type = float;
+#elif ANN_NEURON_CONFIG_TYPE_BYTES == 8
+    using neuron_config_type = double;
+#endif // ANN_NEURON_CONFIG_TYPE_BYTES
+
+#if ANN_OFFSET_TYPE_BYTES == 1
+    using offset_type = uint8_t;
+#elif ANN_OFFSET_TYPE_BYTES == 2
+    using offset_type = uint16_t;
+#elif ANN_OFFSET_TYPE_BYTES == 4
+    using offset_type = uint32_t;
+#elif ANN_OFFSET_TYPE_BYTES == 8
+    using offset_type = uint64_t;
+#endif // ANN_OFFSET_TYPE_BYTES
+
+template<typename TSrc, typename TDst>
+inline TDst Convert(const TSrc value)
 {
     using namespace std::string_literals;
 
-    if (offset > std::numeric_limits<offset_type>::max())
-        throw std::runtime_error("Offset value "s + std::to_string(offset) + " is bigger than offset_type maximum value "s + std::to_string(std::numeric_limits<offset_type>::max()));
+    if (value > std::numeric_limits<TDst>::max())
+    {
+#ifdef ANN_CONVERT_ERROR_ASSERT
+        assert(false);
+#endif // ANN_CONVERT_ERROR_ASSERT
 
-    return static_cast<offset_type>(offset);
+#ifdef ANN_CONVERT_ERROR_THROW
+        throw std::runtime_error("Src data value "s + std::to_string(value) + " is bigger than dst maximum value "s + std::to_string(std::numeric_limits<TDst>::max()));
+#endif // ANN_CONVERT_ERROR_THROW
+    }
+
+    return static_cast<TDst>(value);
+
 }
 
 }   // namespace artificial_neural_network
--- artificial_neural_network/netStructure.cpp
+++ artificial_neural_network/netStructure.cpp
@@ -161,8 +161,8 @@ void CNetStructureImpl::AddNeuronsLayer(EActivationFunction activationFunction, 
     size_t prevLayerSize = CurLayerNeuronsAmount();
 
     SRange newLayerRange;
-    newLayerRange._first = ConvertToOffsetType(_neurons.size());
-    newLayerRange._amount = ConvertToOffsetType(neuronsToAdd);
+    newLayerRange._first = Convert<size_t, TOffset>(_neurons.size());
+    newLayerRange._amount = Convert<size_t, TOffset>(neuronsToAdd);
     _layers.emplace_back(std::move(newLayerRange));
 
     size_t inputsAmount = 0;
@@ -190,9 +190,9 @@ void CNetStructureImpl::AddNeuronsLayer(EActivationFunction activationFunction, 
         SNeuron neuron;
 
         neuron._activationFunction = activationFunction;
-        neuron._layerNeuronPosition = ConvertToOffsetType(i);
-        neuron._inputsAmount = ConvertToOffsetType(inputsAmount);
-        neuron._firstInputOff = ConvertToOffsetType(_inputsOff.size());
+        neuron._layerNeuronPosition = Convert<size_t, TOffset>(i);
+        neuron._inputsAmount = Convert<size_t, TOffset>(inputsAmount);
+        neuron._firstInputOff = Convert<size_t, TOffset>(_inputsOff.size());
 
         for (size_t j = 0; j < inputsAmount; ++j)
         {
@@ -224,7 +224,7 @@ void CNetStructureImpl::SetLastLayerAsOutput()
     checkCondition(CurLayerNeuronsAmount() == _outputs, std::out_of_range, "last neuron layer "s + std::to_string(_layers.size()) + " has to have "s + std::to_string(_outputs) + " neurons that is equal to outputs amount whereas it has "s + std::to_string(CurLayerNeuronsAmount()) + " neurons"s);
 
     for (size_t i = _neurons.size() - _outputs, j = 0; i < _neurons.size(); ++i, ++j)
-        _neurons[i]._stateOff = ConvertToOffsetType(_externalDirBit | j);
+        _neurons[i]._stateOff = Convert<size_t, TOffset>(_externalDirBit | j);
 
     _statesSize -= _outputs;
 }
